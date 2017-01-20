50b3d6de55cda80e977de358fa2e9c59d49d0ade
Daniel Shamoa <shamoa.daniel@gmail.com>
2017-01-20 17:29:34 +0000

    Implemented get_ratios_summary

--- data_reference.py
+++ data_reference.py
@@ -1,10 +1,13 @@
 
+KeyRatiosSummary = 'http://financials.morningstar.com/ajax/exportKR2CSV.html'
+
+
 CASH_FLOWS_STATEMENT = 'cf'
 INCOME_STATEMENT = 'is'
 BALANCE_SHEET_STATEMENT = 'bs'
-AVAILABLE_RANGES = [3, 6, 9, 12]
+_AVAILABLE_RANGES = [3, 6, 9, 12]
 
 def GET_TIMERANGE(months_range):
     
-    if months_range not in AVAILABLE_RANGES:
-        raise WrongQueryParameterException("months_range must be one of :" + str(AVAILABLE_RANGES))
+    if months_range not in _AVAILABLE_RANGES:
+        raise WrongQueryParameterException("months_range must be one of :" + str(_AVAILABLE_RANGES))
--- unofficial.py
+++ unofficial.py
@@ -4,7 +4,7 @@ import requests
 import random
 
 from core import WrongQueryParameterException
-from data_reference import CASH_FLOWS_STATEMENT, INCOME_STATEMENT, BALANCE_SHEET_STATEMENT, AVAILABLE_RANGES, GET_TIMERANGE
+from data_reference import CASH_FLOWS_STATEMENT, INCOME_STATEMENT, BALANCE_SHEET_STATEMENT, AVAILABLE_RANGES, GET_TIMERANGE, 
 
 # key ratios example
 # http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB
@@ -18,20 +18,63 @@ class WrongQueryParameterException(Exception):
 
 class UnofficialTerminal(object):
 
+    """
+    Class that acts as terminal and that performs requests to the API 
+    managing and storing the result of responses.
+    """
+
     def __init__():
         self.current_data = None
         self.current_query = {}
+        self.current_response_text = None
+        self.local_storage = {}
 
-    def _build_url():
-        pass
+    def get_ratios_summary(t, region):
+        """
+        Get request for the ratio summary csv
+        """
+        # design attempt not super sure about this
+        func_params = locals()
 
-    def get_ratios_summary(**kwargs):
+        self.current_query = {KeyRatiosSummary : func_params}
+        response = requests.get(KeyRatioSummary, params=func_params)
+        if response.ok:
+            self.current_response_text = response.text
+        else:
+            self.current_response_text = None
+            ## could use warnings here instead of this
+            print("Error executing request: " + response.reason)
+
+    def last_response_csv_dump(path):
+        """
+        dump the result of the last request to the API
+        in a csv file in the specified file
+        """
+        # find a way to properly output this
+        with open(path, 'w') as outfile:
+            pass
+
+    def last_response_to_json():
+        """
+        return a json from the last received csv table
+        """
         pass
 
     def get_table(**kwargs):
+        """
+        Standard Get request to get the requested financial summary
+        parameters for the request are:
+        -
+        - 
+        """
         self.current_query = kwargs
         pass
 
-    def smart_hacky_get():
-        pass
+    def pp_last_request():
+        """
+        pretty print to the terminal the test from the 
+        last received csv
+        """
+
+
 
